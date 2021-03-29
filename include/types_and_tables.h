4cc591a9161c3ea8c6ba0dbda555a15cfb98f320
Andrey Diakonov <ivredomon@gmail.com>
2021-03-29 12:12:06 +0300

    vers_mistake_in_table

--- include/types_and_tables.h
+++ include/types_and_tables.h
@@ -40,8 +40,8 @@ typedef enum {
 } cjson_event_t;
 
 typedef void (*cjson_callback_t)(cjson_event_t event, const char *s, const char *end, void *data);
-typedef void *(action_t)(char*, char**end, cjson_callback_t callback, void *data);
-//typedef int (*action_t)(const char *s, const char **end, cjson_callback_t callback, void *data);
+//typedef void *(action_t)(char*, char**end, cjson_callback_t callback, void *data);
+typedef int (*action_t)(const char *s, const char **end, cjson_callback_t callback, void *data);
 
 typedef struct {
     state_t state;
@@ -60,12 +60,13 @@ const unsigned int L_COUNT = 7;
 
 const rule_t table[S_COUNT][L_COUNT] = {
                      /*L_CONTENT_TYPE                  L_OTHER_KEYS         L_ENTER        L_NO_ENTER       L_EOF         L_TEXT      */
-/*S_GET_KEY*/        {S_GET_KEY_VALUE,KEY},  {S_GET_KEY_VALUE,values}, {S_ERR,NULL},  {S_ERR,NULL},  {S_ERR,NULL}, {S_ERR,NULL},
-/*S_GET_KEY_VALUE*/  {S_ERR,NULL},              {S_ERR,NULL},             {S_ERR,NULL},  {S_ERR,keys},  {S_ERR,NULL}, {S_ERR,NULL},
+/*S_BEGIN*/           {S_GET_KEY_VALUE,NULL},  {S_GET_KEY_VALUE,NULL}, {S_ERR,NULL},  {S_ERR,NULL},  {S_ERR,NULL}, {S_ERR,NULL},
+/*S_GET_KEY*/        {S_GET_KEY_VALUE,NULL},  {S_GET_KEY_VALUE,NULL}, {S_ERR,NULL},  {S_ERR,NULL},  {S_ERR,NULL}, {S_ERR,NULL},
+/*S_GET_KEY_VALUE*/  {S_ERR,NULL},              {S_ERR,NULL},             {S_ERR,NULL},  {S_ERR,NULL},  {S_ERR,NULL}, {S_ERR,NULL},
 /*S_ENTER*/          {S_ERR,NULL},              {S_ERR,NULL},             {S_ERR,NULL},  {S_ERR,NULL},  {S_EOF,NULL}, {S_TEXT,NULL},
-/*S_TEXT*/           {S_ERR,NULL},              {S_ERR,NULL},             {S_ERR,NULL},  {S_ERR,NULL},  {S_EOF,NULL}, {S_TEXT,text_bound},
+/*S_TEXT*/           {S_ERR,NULL},              {S_ERR,NULL},             {S_ERR,NULL},  {S_ERR,NULL},  {S_EOF,NULL}, {S_TEXT,NULL},
 /*S_EOF*/            {S_ERR,NULL},              {S_ERR,NULL},             {S_ERR,NULL},  {S_ERR,NULL},  {S_END,NULL}, {S_ERR,NULL},
-/*S_END*/            {S_ERR,NULL},              {S_ERR,NULL},             {S_ERR,NULL},  {S_ERR,NULL},  {S_ERR,end},  {S_ERR,NULL}
+/*S_END*/            {S_ERR,NULL},              {S_ERR,NULL},             {S_ERR,NULL},  {S_ERR,NULL},  {S_ERR,NULL},  {S_ERR,NULL}
 };
 
 #endif //DZ_4_PARSER_H
